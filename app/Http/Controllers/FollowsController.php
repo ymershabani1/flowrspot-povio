078188684e6ca51e61d779df2404b8d5bf65620f
ymershabani1 <ymer.shabani@student.cacttus.education>
2021-10-10 01:07:23 +0200

    Editing follow button

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -7,9 +7,12 @@ use Illuminate\Http\Request;
 
 class FollowsController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth');
+    }
     public function store(User $user){
 
-        return $user->username;
+        return auth()->user()->following()->toggle($user->profile);
 
 
     }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -11,7 +11,7 @@
 
                 <div class="d-flex align-items-center pb-3">
                     <div class="h4">{{$user->username}}</div>
-                    <follow-button user-id="{{$user->id}}"></follow-button>
+                    <follow-button user-id="{{$user->id}}" follows="{{$follows}}"></follow-button>
 
                 </div>
 
