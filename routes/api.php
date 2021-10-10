179d1fd5e270d996d3419d16eb4cec6ef6ecd102
ymershabani1 <ymer.shabani@student.cacttus.education>
2021-10-10 23:05:26 +0200

    Implemented API Authentication

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware(['cors', 'json.response', 'auth:api'])->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+//
+//Route::group(['middleware' => ['cors', 'json.response']], function () {
+//    Route::post('/login', [App\Http\Controllers\Api\ApiAuthController::class, 'login']);
+//    Route::post('/register',[App\Http\Controllers\Api\ApiAuthController::class, 'register']);
+//    Route::post('/logout', [App\Http\Controllers\Api\ApiAuthController::class, 'logout']);
+//});
+//
+//Route::middleware('auth:api')->group(function () {
+//    Route::get('/', [App\Http\Controllers\PostsController::class, 'index']);
+//});
+//Route::post('/login', [App\Http\Controllers\Api\ApiAuthController::class, 'login']);
