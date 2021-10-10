67a496967b1b5ac3322dae61a51edf825acbfa51
ymershabani1 <ymer.shabani@student.cacttus.education>
2021-10-11 00:01:42 +0200

    Revert "Revert "Updating Blades""
    This reverts commit 4dfd675e8ac1e91c7344354a03c2f334e423a2d6.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -13,6 +13,14 @@ class PostsController extends Controller
         $this->middleware('auth');
     }
 
+    public function index(){
+        $users = auth()->user()->following()->pluck('profiles.user_id');
+
+        $posts = Post::orderBy('created_at', 'DESC')->get();
+
+        return view('posts.index',compact('posts'));
+    }
+
     public function create(){
         return view('posts.create');
     }
