4dfd675e8ac1e91c7344354a03c2f334e423a2d6
ymershabani1 <ymer.shabani@student.cacttus.education>
2021-10-11 00:00:19 +0200

    Revert "Updating Blades"
    This reverts commit b2f439c7d54a91ed412ae382835c8cc0aa367b3e.

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -27,8 +27,8 @@
 
             <div class="d-flex">
                 <div class="pr-3"><strong>{{$user->posts->count()}}</strong> posts</div>
-                <div class="pr-3"><strong>{{$user->profile->followers->count()}}</strong> followers</div>
-                <div class="pr-3"><strong>{{$user->following->count()}}</strong> following</div>
+                <div class="pr-3"><strong>23k</strong> followers</div>
+                <div class="pr-3"><strong>212</strong> following</div>
             </div>
             <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
             <div>{{$user->profile->description}}</div>
